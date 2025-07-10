e63b848b5fa04485cba25c6f284d3558aa03968d
rbt <422511643@qq.com>
2025-07-11 02:12:56 +0800

    update

--- src/main/java/learn/concurrentSkipListMap/ConcurrentSkipListMapTest.java
+++ src/main/java/learn/concurrentSkipListMap/ConcurrentSkipListMapTest.java
@@ -5,10 +5,14 @@ import java.util.concurrent.ConcurrentSkipListMap;
 public class ConcurrentSkipListMapTest {
 
     public static void main(String[] args) {
-
         //线程安全且有序的map，基于跳跃表实现
-        ConcurrentSkipListMap<Object, Object> concurrentSkipListMap = new ConcurrentSkipListMap<>();
-
+        ConcurrentSkipListMap<Object, Object> map = new ConcurrentSkipListMap<>();
+        map.put(4, "4");
+        map.put(2, "2");
+        map.put(1, "1");
+        map.put(3, "3");
+        //和TreeMap一样，是升序排序的，按key排序
+        map.forEach((k, v) -> System.out.println(k + ":" + v));
     }
 
 }
--- src/main/java/learn/treeMap/TreeMapTest.java
+++ src/main/java/learn/treeMap/TreeMapTest.java
@@ -1,9 +1,21 @@
 package learn.treeMap;
 
+import java.util.TreeMap;
+
 public class TreeMapTest {
 
     public static void main(String[] args) {
-
+        TreeMap<Integer, Object> map = new TreeMap<>();
+        map.put(4, "4");
+        map.put(2, "2");
+        map.put(1, "1");
+        map.put(3, "3");
+        //1:1
+        //2:2
+        //3:3
+        //4:4
+        //TreeMap是升序排序的，按key排序
+        map.forEach((k, v) -> System.out.println(k + ":" + v));
     }
 
 }
